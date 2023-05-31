1b65f21ca90ee9ec1dcc0324f9e383bc6dfac027
Benjamin <BnLvy1@gmail.com>
2023-05-30 18:19:52 -0700

    changes in order to use SQS as trigger

--- index.ts
+++ index.ts
@@ -1,63 +1,68 @@
+import {
+  DeleteMessageCommand,
+  SQSClient,
+} from "@aws-sdk/client-sqs";
 import { backOff, BackoffOptions } from "exponential-backoff";
-
-function getWeather() {
-  return fetch("weather-endpoint");
-}
-
-async function main() {
-  try {
-    const response = await backOff(() => getWeather());
-    // process response
-  } catch (e) {
-    // handle error
-  }
-}
-
-main();
-
-
-
-
-
+import {SQSEvent} from 'aws-lambda'
 import fetch from 'node-fetch'
 import getSecret from './libs/getSecret'
-exports.handler = async (event, context, callback) => {
+exports.handler = async (event: SQSEvent, context, callback) => {
+  const body = event.Records[0].body
 
+  const discordChannel = JSON.parse(body).channel
   const discordToken = await getSecret('production')
-  const url = `https://discord.com/api/v10/channels/803429347750576141/messages`
+  const url = `https://discord.com/api/v10/channels/${discordChannel}/messages`
   const backoffOptions: BackoffOptions = {
-    jitter: "full",
+    jitter: "none",
     numOfAttempts: 5,
-    maxDelay: 20
-  }
+    delayFirstAttempt: false,
+    startingDelay: 2000
+  } // options explained: https://www.npmjs.com/package/exponential-backoff
+
   const response = await backOff(()=>
     fetch(url, {
 	method: 'post',
-	body: JSON.stringify(body),
+	body: JSON.parse(body).embeds,
 	headers: {
           'Content-Type': 'application/json',
           'Authorization': discordToken
         }
-     })
+     }),
+    backoffOptions
   )
-
-  if([401,403].includes(response.status)){
+  if([401,403,404].includes(response.status)){
     throw new HTTPResponseError(response)
+  } // see "Invalid Request Limit aka Cloudflare bans" https://discord.com/developers/docs/topics/rate-limits
+
+  const client = new SQSClient({});
+  const SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
+  const recieptHandle = event.Records[0].receiptHandle
+
+  if([200,201,204].includes(response.status)){
+        await client.send(
+          new DeleteMessageCommand({
+            QueueUrl: SQS_QUEUE_URL,
+            ReceiptHandle: recieptHandle,
+          })
+        )
+  } // all three of these codes indicate success https://discord.com/developers/docs/topics/opcodes-and-status-codes#http
+
+  const headerObj = {}
+  for(const header of response.headers){
+    headerObj[header[0]] = header[1]
   }
-  return response.status
-  const data = await response.json();
-  return data
+  console.log(headerObj)
+  return {status: response.status, statusText: response.statusText}
 }
 
 class HTTPResponseError extends Error {
 	constructor(response) {
 		super(`HTTP Error Response: ${response.status} ${response.statusText}`);
-		this.response = response;
 	}
 }
+"Delete a message from an Amazon SQS queue using an AWS SDK"
 
-
-const body = {"embeds":[{"title":"Assessment of equity and efficiency of magnetic resonance imaging services in Henan Province, China","description":"CONCLUSIONS: Although the overall equity of configuration at the provincial level is relatively good, equity varies at the municipal level. Our results demonstrate a low MRI utilization efficiency; accordingly, policymakers should dynamically adjust the policy based on equity and efficiency.","author":{"name":"Xiaoling Huang et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221536"},{"title":"Characteristics of idiopathic pulmonary fibrosis -associated cough. a case-control study","description":"CONCLUSION: Cough in early stage IPF patients was not distinguishable from chronic cough in the community-based population by LCQ. Especially, there was no difference in the self-reported frequency of cough-associated sputum production.","author":{"name":"Eeva Saari et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221535"},{"title":"Oral contraceptive pills shortage in Lebanon amidst the economic collapse: a nationwide exploratory study","description":"CONCLUSIONS: OCPs shortage has seriously and negatively exposed women to various undesirable consequences including unplanned pregnancy and dysregulation of menses. Therefore, there is an urgent need to bring the attention of healthcare authorities to support the national pharmaceutical industry in manufacturing affordable OCPs generics to meet women's reproductive health demands.","author":{"name":"Rania Itani et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221534"},{"title":"Influence of Er:YAG laser irradiation on the outcomes of alveolar ridge preservation at the infected molar sites: a randomized controlled trial","description":"CONCLUSIONS: ARP with Er:YAG laser irradiation seemed to improve bone healing by regulating osteogenesis-related factor expression in the early stage at infected sites.","author":{"name":"Yong Zhou et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221532"},{"title":"Assessing the impact of a knowledge translation intervention on physical therapists' self-efficacy and implementation of motor learning practice","description":"CONCLUSIONS: Findings support the positive effect of an educational tool, most prominently on physical therapists' ML self-efficacy. The addition of practical modeling or ongoing educational support may enhance intervention effects.","author":{"name":"Michal Kafri et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221530"},{"title":"Estimation of health literacy levels in patients with cardiovascular diseases in a Gulf country","description":"CONCLUSION: The inadequate HL levels found in outpatients with CVD is a major health concern in the UAE. To improve population health outcomes, health system interventions, including targeted educational and behavioral programs for the older population are necessary.","author":{"name":"Satish Chandrasekhar Nair et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221529"},{"title":"The efficacy of manual toothbrushes in patients with fixed orthodontic appliances: a randomized clinical trial","description":"CONCLUSIONS: Plaque was significantly removed by the conventional FT toothbrush after a single brushing compared to the OT and CA types.","author":{"name":"Fathima Fazrina Farook et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221525"},{"title":"Comparison of unipedicular and bipedicular kyphoplasty for treating acute osteoporotic vertebral compression fractures in the lower lumbar spine: a retrospective study","description":"CONCLUSIONS: The clinical and radiological results of unipedicular percutaneous kyphoplasty for treating osteoporotic vertebral compression fractures in the lower lumbar region were similar to those of bipedicular percutaneous kyphoplasty. However, the unipedicular approach resulted in shorter surgical time, less blood loss, and less bone cement leakage. Thus, the unipedicular approach may be preferable owing to its several advantages.","author":{"name":"Yunfan Qian et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221523"},{"title":"Epidemiology of maxillofacial fractures in northwest China: an 11-year retrospective study of 2240 patients","description":"CONCLUSIONS: The maxillofacial fracture pattern is correlated with sex, age and aetiology. Patients were mainly young and middle-aged males, and the main cause of injury was RTAs, mostly causing compound fractures. Medical staff must be systematically educated to comprehensively examine patients with injuries resulting from RTAs. The management of patients with fractures requires thorough consideration of the patient's age, aetiology, fracture site, and concomitant injuries.","author":{"name":"Jingjing Mao et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221520"},{"title":"Evaluation of immunogenicity of gene-deleted and subunit vaccines constructed against the emerging pseudorabies virus variants","description":"CONCLUSIONS: The SD-2017ΔgE/gI/TK live attenuated vaccine provided 100% protection against PRV variant challenge. Interestingly, the subunit vaccines with gB protein linked to DCpep and PorB protein as adjuvant may also be a promising and effective PRV variant vaccine candidate.","author":{"name":"Hong-Liang Zhang et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221518"}],"server":"803429347750576138","channel":"1077344605688823869"}
+const body = {"embeds":[{"title":"Assessment of equity and efficiency of magnetic resonance imaging services in Henan Province, China","description":"CONCLUSIONS: Although the overall equity of configuration at the provincial level is relatively good, equity varies at the municipal level. Our results demonstrate a low MRI utilization efficiency; accordingly, policymakers should dynamically adjust the policy based on equity and efficiency.","author":{"name":"Xiaoling Huang et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221536"},{"title":"Characteristics of idiopathic pulmonary fibrosis -associated cough. a case-control study","description":"CONCLUSION: Cough in early stage IPF patients was not distinguishable from chronic cough in the community-based population by LCQ. Especially, there was no difference in the self-reported frequency of cough-associated sputum production.","author":{"name":"Eeva Saari et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221535"},{"title":"Oral contraceptive pills shortage in Lebanon amidst the economic collapse: a nationwide exploratory study","description":"CONCLUSIONS: OCPs shortage has seriously and negatively exposed women to various undesirable consequences including unplanned pregnancy and dysregulation of menses. Therefore, there is an urgent need to bring the attention of healthcare authorities to support the national pharmaceutical industry in manufacturing affordable OCPs generics to meet women's reproductive health demands.","author":{"name":"Rania Itani et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221534"},{"title":"Influence of Er:YAG laser irradiation on the outcomes of alveolar ridge preservation at the infected molar sites: a randomized controlled trial","description":"CONCLUSIONS: ARP with Er:YAG laser irradiation seemed to improve bone healing by regulating osteogenesis-related factor expression in the early stage at infected sites.","author":{"name":"Yong Zhou et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221532"},{"title":"Assessing the impact of a knowledge translation intervention on physical therapists' self-efficacy and implementation of motor learning practice","description":"CONCLUSIONS: Findings support the positive effect of an educational tool, most prominently on physical therapists' ML self-efficacy. The addition of practical modeling or ongoing educational support may enhance intervention effects.","author":{"name":"Michal Kafri et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221530"},{"title":"Estimation of health literacy levels in patients with cardiovascular diseases in a Gulf country","description":"CONCLUSION: The inadequate HL levels found in outpatients with CVD is a major health concern in the UAE. To improve population health outcomes, health system interventions, including targeted educational and behavioral programs for the older population are necessary.","author":{"name":"Satish Chandrasekhar Nair et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221529"},{"title":"The efficacy of manual toothbrushes in patients with fixed orthodontic appliances: a randomized clinical trial","description":"CONCLUSIONS: Plaque was significantly removed by the conventional FT toothbrush after a single brushing compared to the OT and CA types.","author":{"name":"Fathima Fazrina Farook et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221525"},{"title":"Comparison of unipedicular and bipedicular kyphoplasty for treating acute osteoporotic vertebral compression fractures in the lower lumbar spine: a retrospective study","description":"CONCLUSIONS: The clinical and radiological results of unipedicular percutaneous kyphoplasty for treating osteoporotic vertebral compression fractures in the lower lumbar region were similar to those of bipedicular percutaneous kyphoplasty. However, the unipedicular approach resulted in shorter surgical time, less blood loss, and less bone cement leakage. Thus, the unipedicular approach may be preferable owing to its several advantages.","author":{"name":"Yunfan Qian et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221523"},{"title":"Epidemiology of maxillofacial fractures in northwest China: an 11-year retrospective study of 2240 patients","description":"CONCLUSIONS: The maxillofacial fracture pattern is correlated with sex, age and aetiology. Patients were mainly young and middle-aged males, and the main cause of injury was RTAs, mostly causing compound fractures. Medical staff must be systematically educated to comprehensively examine patients with injuries resulting from RTAs. The management of patients with fractures requires thorough consideration of the patient's age, aetiology, fracture site, and concomitant injuries.","author":{"name":"Jingjing Mao et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221520"},{"title":"Evaluation of immunogenicity of gene-deleted and subunit vaccines constructed against the emerging pseudorabies virus variants","description":"CONCLUSIONS: The SD-2017ΔgE/gI/TK live attenuated vaccine provided 100% protection against PRV variant challenge. Interestingly, the subunit vaccines with gB protein linked to DCpep and PorB protein as adjuvant may also be a promising and effective PRV variant vaccine candidate.","author":{"name":"Hong-Liang Zhang et al."},"url":"https://pubmed.ncbi.nlm.nih.gov/37221518"}],"server":"803429347750576138","channel":"1112162476923039837" }
 const oldBody = {"embeds":
 [
      {
